7c8d50696fe2d2ba6b8ef25255f4522b1e10d984
MaiconMW <maicon.w312@outlook.com>
2026-09-09 20:37:03 -0300

    Page Liberados

--- ManutencaoWeb/obj/Debug/net8.0/ManutencaoWeb.AssemblyInfo.cs
+++ ManutencaoWeb/obj/Debug/net8.0/ManutencaoWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManutencaoWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4d4ec3546a7074bdc80fe08858447f639d2c30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f2da02f735e2a633a516ccf77f79ef50ff3eea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManutencaoWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManutencaoWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
